d81b2e8c4d7fc0024932b3a18617c28a70b61249
Jeedx <elton.flores.a@uni.pe>
2020-05-26 22:50:40 -0500

    agrego el esqueleto de contact

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 const nav__item = document.getElementById('nav__item');
 const alturavp  = window.innerHeight;
 const container = document.getElementById('container');
-container.style.height=`${alturavp-116}px`
-;
+container.style.height=`${alturavp-116}px`;
 const typed = new Typed('.element', {
     strings: ['<p class="txt"> Hi, I am Bryan Flores Avila, student of the National University of Engineering and my hobby is creating web pages.</p>'],
     typeSpeed: 50,
