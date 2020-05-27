d96bcf53614ed7645406575e6f60c3e651b1b5a3
Jeedx <elton.flores.a@uni.pe>
2020-05-27 04:00:38 -0500

    Se 'arreglo' el rezize de las paginas

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
-const nav__item = document.getElementById('nav__item');
-const alturavp  = window.innerHeight;
-const container = document.getElementById('container');
-container.style.height=`${alturavp-116}px`;
-const typed = new Typed('.element', {
-    strings: ['<p class="txt"> Hi, I am Bryan Flores Avila, student of the National University of Engineering and my hobby is creating web pages.</p>'],
-    typeSpeed: 50,
-    loop:false,
-    showCursor:true,
-    cursorChar:'|',
-    contentType:'html',
-  });
-
-
-
+window.addEventListener("resize", (e) => {
+  console.log(e.srcElement.innerHeight);
+  console.log(e.srcElement.innerWidth);
+  const container = document.getElementById("container");
+  container.style.height = `${e.srcElement.innerHeight - 116}px`;
+});
+const nav__item = document.getElementById("nav__item");
+const alturavp = window.innerHeight; 
+container.style.height = `${alturavp- 116}px`;
+const typed = new Typed(".element", {
+  strings: [
+    '<p class="txt"> Hi, I am Bryan Flores Avila, student of the National University of Engineering and my hobby is creating web pages.</p>',
+  ],
+  typeSpeed: 50,
+  loop: false,
+  showCursor: false,
+  /* cursorChar: "|", */
+  contentType: "html",
+});
