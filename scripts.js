a0f6ff49c8989a8333e35b25e5aef3ae1d670d8a
Jeedx <elton.flores.a@uni.pe>
2020-05-29 08:05:09 -0500

    index responsive 90%

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
-window.addEventListener("resize", (e) => {
-  console.log(e.srcElement.innerHeight);
-  console.log(e.srcElement.innerWidth);
-  const container = document.getElementById("container");
-  container.style.height = `${e.srcElement.innerHeight - 116}px`;
-  console.log('aun sigo detectando');
-});
-const nav__item = document.getElementById("nav__item");
+const wrapper = document.getElementById('wrapper');
 const alturavp = window.innerHeight; 
-container.style.height = `${alturavp- 116}px`;
+wrapper.style.height = `${alturavp- 217}px`;
+window.addEventListener("resize", (e) => {
+    console.log(e.srcElement.innerHeight);
+    console.log(e.srcElement.innerWidth);
+    const container = document.getElementById("wrapper");
+    container.style.height = `${e.srcElement.innerHeight - 217}px`;
+    console.log('aun sigo detectando');
+  });
+const navbarclose=document.getElementById('navbar');
+const navbarOpen=document.getElementById('open');
+const openNavbar=()=> {
+    navbarclose.style.width = "100%";
+    navbarOpen.style.opacity = 0;
+  }
+  const closeNavbar=()=>{
+    navbarclose.style.width = "0";
+    navbarOpen.style.opacity = 1;
+  }
+  
+navbarOpen.addEventListener('click',openNavbar);
+navbarclose.addEventListener('click',closeNavbar);
+
 const typed = new Typed(".element", {
   strings: [
     '<p class="txt"> Hi, I am Bryan Flores Avila, student of the National University of Engineering and my hobby is creating web pages.</p>',
