af962bb2853fe3bdb7fb18c6622ae2b9b86604f1
Jeedx <elton.flores.a@uni.pe>
2020-05-29 22:00:06 -0500

    Nuevo menu hamburguer

--- scripts.js
+++ scripts.js
@@ -1,31 +1,44 @@
-const navbarclose=document.getElementById('navbar');
-const navbarOpen=document.getElementById('open');
-const openNavbar=()=> {
-    navbarclose.style.width = "100%";
-    navbarOpen.style.opacity = 0;
+const line = document.getElementById("toggle__line");
+const toggle = document.getElementById("toggle");
+const check = document.getElementById("check");
+const navbarclose = document.getElementById("navbar");
+console.log(check.checked);
+const openNavbar = () => {
+  navbarclose.style.width = "100%";
+};
+const closeNavbar = () => {
+  navbarclose.style.width = "0%";
+};
+
+toggle.addEventListener("click", () => {
+  if (check.checked == true) {
+    toggle.style.transition = "all 0.3s";
+    toggle.children[0].style.transform = "rotate(45deg) translate(5px, 9px)";
+    toggle.children[1].style.opacity = 0;
+    toggle.children[2].style.transform = "rotate(-45deg) translate(5px, -9px)";
+    openNavbar();
+  } else {
+    toggle.children[0].style.transform = "none";
+    toggle.children[1].style.opacity = 1;
+    toggle.children[2].style.transform = "none";
+
+    closeNavbar();
   }
-  const closeNavbar=()=>{
-    navbarclose.style.width = "0";
-    navbarOpen.style.opacity = 1;
-  }
-  
-navbarOpen.addEventListener('click',openNavbar);
-navbarclose.addEventListener('click',closeNavbar);
+});
 
-
-
-const wrapper = document.getElementById('wrapper');
-const alturavp = window.innerHeight; 
-wrapper.style.height = `${alturavp- 167}px`;
+/*---------CALCULA EL VP ........*/
+const wrapper = document.getElementById("wrapper");
+const alturavp = window.innerHeight;
+wrapper.style.height = `${alturavp - 167}px`;
 window.addEventListener("resize", (e) => {
-    console.log(e.srcElement.innerHeight);
-    console.log(e.srcElement.innerWidth);
-    const container = document.getElementById("wrapper");
-    container.style.height = `${e.srcElement.innerHeight - 167}px`;
-    console.log('aun sigo detectando');
-  });
+  console.log(e.srcElement.innerHeight);
+  console.log(e.srcElement.innerWidth);
+  const container = document.getElementById("wrapper");
+  container.style.height = `${e.srcElement.innerHeight - 167}px`;
+  console.log("aun sigo detectando");
+});
 
-
+/*---------EFECTO DE LA LETRA........*/
 const typed = new Typed(".element", {
   strings: [
     '<p class="txt"> Hi, I am Bryan Flores Avila, student of the National University of Engineering and my hobby is creating web pages.</p>',
